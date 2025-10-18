d4c5e20487f615603d4131921b522851580da2f6
RAKIB6348 <shahidulhuda0@gmail.com>
2025-10-18 22:52:07 +0600

    create static and media file config

--- hotel_management/hotel_management/settings.py
+++ hotel_management/hotel_management/settings.py
@@ -135,6 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # for production
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # for development
+
+
+MEDIA_URL = 'media/' # browser access path
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Uploaded files saved folder
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
